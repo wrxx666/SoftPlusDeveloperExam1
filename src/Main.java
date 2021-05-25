586a92bc777d252c7e7ff46e730f9430609e1acd
wrxx666 <70403604+wrxx666@users.noreply.github.com>
2021-05-26 01:24:54 +0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,46 @@
-/*
-public class Main {
-    public static void main(String[] args){
-        Employee tester = new Employee(7, "Developer");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vacancy developer = new Vacancy("Developer");
+        Employee tester = new Employee(7L, developer);
         System.out.println(tester.getId());
         System.out.println(tester.getVacancy());
     }
 }
 
-class Employee {
+class Employee
+{
     private long id;
-    private String vacancy;
+    private Vacancy vacancy;
 
-    public Employee(long id, String vacancy) {
+    Employee(long id,Vacancy vacancy)
+    {
         this.id = id;
         this.vacancy = vacancy;
     }
 
-    String getVacancy() {
-        return vacancy;
+    String getVacancy()
+    {
+        if (vacancy == null) return null;
+        else return vacancy.getVacancyName();
     }
 
-    long getId() {
-        return id;
-    }
-}
-*/
-
-public class Main {
-    public static void main(String[] args){
-        Employee tester = new Employee(7, "Developer");
-        System.out.println(tester.getId());
-        System.out.println(tester.getVacancy());
-    }
-}
-
-class Employee {
-    private long id;
-    private Vacancy vacancy;
-    Employee(long id,String vacancyName){
-        this.id = id;
-        this.vacancy = new Vacancy(vacancyName);
-    }
-    String getVacancy() {
-        return vacancy.getVacancyName();
-    }
-
-    long getId() {
+    long getId()
+    {
         return id;
     }
 }
 
-class Vacancy {
+class Vacancy
+{
     private String vacancyName;
-    Vacancy(String vacancyName){
+    Vacancy(String vacancyName)
+    {
         this.vacancyName = vacancyName;
     }
-    String getVacancyName() {
+    String getVacancyName()
+    {
         return vacancyName;
     }
 }
